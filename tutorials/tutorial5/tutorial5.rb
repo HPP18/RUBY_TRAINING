18c92e767c33f6df7f74a44b3eb424746122ec89
hpp18 <hninpwintphyu93287@gmail.com>
2022-02-18 15:44:23 +0630

    [feature/tutorial_05] tutorial5 updated

--- tutorials/tutorial5/tutorial5.rb
+++ tutorials/tutorial5/tutorial5.rb
@@ -1,13 +1,12 @@
 require "date"
 
 d = Date.today
-arr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 a = 5
 
 while a > 0
-  arr.length.times do |i|
+  7.times do |i|
     if d.prev_day(a).wday === i
-      p arr[i]
+      p d.prev_day(a).strftime("%A")
     end
   end
   a = a - 1
