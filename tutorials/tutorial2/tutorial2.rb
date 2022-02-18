509864e8fa316d18343f05c5b4ffd2c0636420d5
hpp18 <hninpwintphyu93287@gmail.com>
2022-02-18 14:55:57 +0630

    [feature/tutorial_02] tutorial2 updated

--- tutorials/tutorial2/tutorial2.rb
+++ tutorials/tutorial2/tutorial2.rb
@@ -1,9 +1,14 @@
-require 'date'
+require "date"
 
 def age(dob)
-  now = Time.now.utc.to_date
-  age =  now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
-  print age, (age > 18 ? ', adult' : ', child')
+  if dob < Time.now.utc.to_date
+    now = Time.now.utc.to_date
+    age = now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+    p age, (age > 18 ? "adult" : "child")
+  else
+    p "your age is invalid"
+  end
 end
+
 puts "Enter Your Date of Birth(Ex:1990/10/16)"
-age(DateTime.parse(gets.chomp))
+age(Date.parse(gets.chomp))
