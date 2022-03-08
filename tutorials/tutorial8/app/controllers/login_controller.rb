6af0e4e36ecec2542c51a2d37300895c079af052
hpp18 <hninpwintphyu93287@gmail.com>
2022-03-08 12:55:37 +0630

    [feature/tutorial_08] tutorial8 uploaded

--- tutorials/tutorial8/app/controllers/login_controller.rb
+++ tutorials/tutorial8/app/controllers/login_controller.rb
@@ -2,15 +2,15 @@ class LoginController < ApplicationController
   def setlogin
     if EMAIL === params[:email] && PASSWORD === params[:password]
       session[:email] = EMAIL
-      redirect_to root_path, notice: "Logged in successfully"
+      redirect_to root_path, notice: SUCCESS
     else
-      flash[:alert] = "Invalid email or password"
+      flash[:alert] = INVALID
       render :login
     end
   end
 
   def setlogout
     session[:email] = nil
-    redirect_to login_path, notice: "Logged out"
+    redirect_to login_path, notice: LOGOUT
   end
 end
